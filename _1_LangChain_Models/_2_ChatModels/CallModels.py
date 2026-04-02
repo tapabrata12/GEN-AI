4a3581d6ee45282b7bf2f3f3b286969cb53c9eaf
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2026-04-02 17:41:12 +0530

    Deleted Everything

--- _1_LangChain_Models/_2_ChatModels/CallModels.py
+++
@@ -1,9 +0,0 @@
-from langchain.chat_models import init_chat_model
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = init_chat_model(model_provider="google_genai",model="gemini-3-flash-preview", temperature=0.7)
-
-result = model.invoke("What is the capital of India ?")
-print(result.content)
